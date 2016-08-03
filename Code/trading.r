6748510398307aeaaf9850fa9401d216246035dc
claudiu <claudiu.schiller@gmail.com>
2016-08-03 11:48:55 +0300

    trading flow created

--- Code/trading.r
+++ Code/trading.r
@@ -4,6 +4,24 @@ library(xts)
 library(ggplot2)
 
 
+#source all modules and execute before trading
+source("Code/StockDataBuilder.R")
+source("Code/K_Histogram.R")
+DDate <- as.Date("2016/1/1")
+K_Histogram(K=20,DDate=DDate)
+source("Code/Classification.R")
+source("Code/match.r")
+DDate <- as.Date("2016/5/1")
+k <- 20 #how many days to use for k parameter
+l <- 10 #number of different classes used in classification
+matchfunc(DDate,k,l)
+source("Code/strategy.r")
+DDate <- as.Date("2016/5/1")
+k <- 20 #how many days to use for k parameter
+l <- 10 #number of different classes used in classification
+beststrat(DDate,k,l)
+
+#trading backtest
 load("DataWork/StockPrices.Rdata")
 retcolnames <- grep("return",colnames(StockPrices))
 nstocks <- length(retcolnames)
