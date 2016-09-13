1a857c6896d06cc27a4fa9dfd86cf97a51a5b5f1
OzPirvandy <pirvandy@gmail.com>
2016-09-13 08:30:40 +0300

    adding elbow to clara

--- Code/Classification.R
+++ Code/Classification.R
@@ -4,7 +4,7 @@
 library("cluster")
 #library("randomForest")
 
-Classification<-function (KVec ,K = 20 , L = 8 , KKR = "Monkey"){
+Classification<-function (KVec ,K = 20 , L = 8 , KKR = "Monkey",optimize = 0){
 
   
   #All available featuress :  K-means & Monkey
@@ -27,16 +27,40 @@ Classification<-function (KVec ,K = 20 , L = 8 , KKR = "Monkey"){
   
  
   #Part B, cheaking the Classification:
-  Flag <- which (CLASS == KKR)  #K-means, classifier, randomforest, Monkey ; c(1,2,3,4)
+  
+  #############################
+  ## optimize clara / elbow  ##
+  #############################
+  
+  ClaraOptimzation<-function(Critical_Value = 0.1){
+    paste0(timestamp(),"Start Opimization")
+    #Critical_Value is the decision point of improvements to 
+    #set the number of K in el for opimize the classification.
+    set.seed(176)
+    n_centers = 1:20  #numcer of center we shell check. 
+    Cost_elbow<-sapply(n_centers,function(x) clara(t(DataSet),x,samples=500
+                        ,sampsize = nrow(t(DataSet)),metric="manhattan",pamLike=T)$objective)
+    #plot(n_centers,log(Cost_elbow))
+    A<-sqrt((Cost_elbow[-20]-Cost_elbow[-1])^2)  #calculating absolute distance between Costs
+    B<-abs((A[-1]-A[-19])/A[-1])   #calculating the precentage of accuracy 
+    Opitmum_Of_Clara<-min(which(B<Critical_Value))+2  # Find the first num which obey the restriction
+    #plot(abs(B))                                    # have to add 2 for matcing the indices.
+    paste0(timestamp(),"end optinization")
+    return(Opitmum_Of_Clara)
+  }
+  
+  if(optimize==1) L<-ClaraOptimzation()
+  
+  ####################
+  # End Of optimize  #
+  ####################
+  
+  #Flag <- which (CLASS == KKR)  #K-means, classifier, randomforest, Monkey ; c(1,2,3,4)
+  Flag <- switch(KKR, "K-means"=1, "Clara"=2, "Kernel"=3, "Randomforest"=4, "Monkey"=0)
   
   if (Flag == 1){
-    #for (o in 1:10){
-    #options(warn=-1)  #stop getting warnings in R about the "converge in 10 iterations"
     set.seed(123)
-    #Class <- kmeans(t(DataSet[,IndexOut]),L , nstart = L,iter.max = 20 )#
     Class <- kmeans(t(DataSet[,IndexOut]),L , nstart = L,iter.max = 20 )#
-    #Chanig to matrix!!!!!!!!!!!!!!!!!!ss
-    #options(warn=0)   #keep getting warnings about problems
     Classifier<-cbind.data.frame(KVec,Class=Class$cluster)
     ClassCenters<-Class$centers
     rownames(ClassCenters) <- paste("center",1:L,sep = "")
@@ -44,20 +68,41 @@ Classification<-function (KVec ,K = 20 , L = 8 , KKR = "Monkey"){
   }#end if Flag == 1;
   
   
+  # Correlations_clastering<-function(){
+  #   Windows<-1:(length(KVec)-K) #number of moving windows.
+  #   
+  #   CorrData<-sapply(Windows, function(x){
+  #                   Correlation<-cor(StockPrices[x:(x+K-1),1:13])
+  #                   CorrelationIndices<-lower.tri(Correlation, diag = FALSE)
+  #            לבדוק את הנושא לעומק לפני שימוש במטריצה זו       UsefulCorrelations<-c(Correlation[CorrelationIndices])
+  #                   return(UsefulCorrelations)
+  #                   })
+  #     
+  #   ###########need to rm the cash from stick data.
+  # }
+  # 
+  
+  
+  
+  
+  
+  
+  
   
   if (Flag == 2) {
     set.seed(176)
-    #Class <- svm (DataSet~DataSet[,1],type = "C-classification",data = DataSet,gamma = 0.1)
     Class <- clara(t(DataSet),L,samples=500,sampsize = nrow(t(DataSet)),metric="manhattan",pamLike=T)
     Classifier<-cbind.data.frame(KVec,Class=Class$clustering)
     #hist(Classifier$Class)
     ClassCenters<-Class$medoids
     rownames(ClassCenters)<-paste("center",1:L,sep = "")
+    
     colnames(ClassCenters)<-paste("Day",1:ncol(ClassCenters),sep = "")
-    save(ClassCenters,file = "DataWork/ClassCenters.Rdata")
+    centers_file_name<-paste0("DataWork/ClassCenters",K,".Rdata")
+    save(ClassCenters,file = centers_file_name)
   }
   
-  if (Flag == 4){ #Monkey
+  if (Flag == 0){ #Monkey
     Num_Of_Windowdim<-length(KVec)
     Class <-round(runif(Num_Of_Windowdim,1,L))
     Classifier<-cbind.data.frame(KVec,Class)
@@ -66,6 +111,7 @@ Classification<-function (KVec ,K = 20 , L = 8 , KKR = "Monkey"){
   print(paste0("Classifier working, k is: ",K))
   filename <- paste0("DataWork/Classifier",K,".Rdata")
   save(Classifier,file=filename)
+  
   return(Classifier)
 
 }
